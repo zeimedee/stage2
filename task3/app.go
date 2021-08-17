c19ca8c34393150b4e8146b9a4f417d6840bc88e
alexander <zeimedee@gmail.com>
2021-08-17 15:17:25 +0000

    stage 2 task changes

---
+++ task3/app.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+type Mes struct {
+	Name    string `json:"name" form:"name"`
+	Email   string `json:"email" form:"email"`
+	Subject string `json:"subject" form:"subject"`
+	Message string `json:"message" form:"message"`
+}
+
+func hi(c *fiber.Ctx) error {
+	return c.SendString("hi")
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{
+		"Title":   "hello world",
+		"Message": "alexander",
+	})
+}
+
+func sub(c *fiber.Ctx) error {
+	mes := new(Mes)
+	err := c.BodyParser(mes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.Render("index", fiber.Map{
+		"Sub": mes.Name,
+	})
+}
+
+func handlers(app *fiber.App) {
+	app.Get("/hi", hi)
+
+	app.Get("/", hello)
+
+	app.Post("/", sub)
+}
+
+func main() {
+	engine := html.New("./public", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	handlers(app)
+
+	log.Fatal(app.Listen(":4000"))
+}
---
+++ task3/go.mod
@@ -0,0 +1,8 @@
+module github.com/zeimedee/stage2
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.17.0 // indirect
+	github.com/gofiber/template v1.6.15
+)
